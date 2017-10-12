349c8ab4713172aa2a87d0990fe29765daa734c4
modrake <12264057+modrake@users.noreply.github.com>
2017-10-11 17:15:43 -0700

    ch1-1

---
+++ ch1-1.cpp
@@ -0,0 +1,7 @@
+/* function change: calculates change in USD coins to give a customer
+   prints output to stdout. Checks for purchase price being higher 
+   than paid price and prints error notice if so.
+   @params: int d-paid, c-paid: dollars and cents paid to cashier
+                d-cost, c-cost: dollars and cents of cost of item
+   @post: change to give customer printed to stdout.
+*/
