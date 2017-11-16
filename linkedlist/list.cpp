fefcdfa5f792d9178a9ee1052be15a779d513ddf
modrake <12264057+modrake@users.noreply.github.com>
2017-11-16 14:57:31 -0800

    flipeveryother done

--- linkedlist/list.cpp
+++ linkedlist/list.cpp
@@ -21,9 +21,46 @@ void printList (Node *head) {
   }
 }
 
+
 int countItems (Node *head) {
   if (head == nullptr) return 0;
   else return (1 + countItems(head->next));
+}
+
+void flipEveryOther (Node *&head) {
+    Node *cur = head;
+    if ((head != nullptr) && (head->next != nullptr)) {
+      Node *b = cur->next;
+      cur->next = b->next;
+      b->next = cur;
+      head = b;
+      delete b;
+    }
+    while ((cur->next != nullptr) && (cur->next->next != nullptr)) {
+      std::cout << "* " << cur->data;
+      Node *a = cur->next;
+      Node *b = cur->next->next;
+      cur->next = b;
+      a->next = b->next;
+      b->next = a;
+      cur = cur->next->next;
+      //delete a;
+      //delete b;
+      std::cout << "& " <<std::endl;
+    }
+}
+
+void flipEveryOtherR (Node *&head) {
+  if ((head==nullptr) || (head->next == nullptr)) return;
+  else{
+    Node *temp = new Node;
+    temp = head;
+    head = head->next;
+    temp->next = head->next;
+    head->next = temp;
+    flipEveryOtherR (head->next->next);
+    delete temp;
+  }
 }
 
 int main() {
@@ -32,9 +69,13 @@ int main() {
   head->next=nullptr;
   addItem(head,2);
   addItem(head,3);
+  addItem(head,4);
+  addItem(head,5);
+  addItem(head,6);
+  addItem(head,7);
+  //addItem(head,8);
 
-  cout << countItems (head) << endl;
-  printList(head);
+  flipEveryOther (head);
   printList(head);
   return 0;
 }
